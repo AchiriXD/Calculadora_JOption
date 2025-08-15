b9fa8c4bab42c0115f809435a8322b504d108f90
DAchiri <coopercol77@gmail.com>
2025-08-15 18:11:32 -0500

    version 3 (se agrego el historial)

--- Calculadora.java
+++ Calculadora.java
@@ -1,5 +1,6 @@
 // Calculadora en JOptionPane
 // Autor : Daniel Achiri 
+import java.util.*;
 import javax.swing.*;
 public class Calculadora {
     public static void main(String[] args){
@@ -7,9 +8,10 @@ public class Calculadora {
         double num1 = 0, num2, resultado = 0;
         int seleccion;
         boolean conservar = false, operador_validez = true;
-        String num1_string, operacion;
+        String num1_string, operador, operacion = "";
+        ArrayList <String> historial = new ArrayList<String>();
         JOptionPane.showMessageDialog(null, "---Bienvenido a la Calculadora---\n-Solo dos numeros.\n-Operaciones: Suma, Resta, Multiplicación, Divición, Porcentaje y Módulo"
-        +"\n-Se puede conservar o descartar un numero despues de una operacion");
+        +"\n-Se puede conservar o descartar un numero despues de una operacion\n-Al final se mostrara el historial de las operaciones");
         while (confirmacion) {
             if (conservar){
                 num1 = resultado;
@@ -18,9 +20,9 @@ public class Calculadora {
                 num1_string = JOptionPane.showInputDialog("Ingrese el primer numero: ");
                 num1 = Double.parseDouble(num1_string);
             }
-            operacion = (JOptionPane.showInputDialog("Ingrese la operacion\nSuma( + )\nResta( - )\nMultiplicación( * )\nDivición( / )\nPorcentaje ( % )\nMódulo( mod )").trim());
+            operador = (JOptionPane.showInputDialog("Ingrese la operacion\nSuma( + )\nResta( - )\nMultiplicación( * )\nDivición( / )\nPorcentaje ( % )\nMódulo( mod )").trim());
             num2 = input_numero2("Ingrese el segundo numero");
-            switch (operacion) {
+            switch (operador) {
                 case "+" :
                 resultado = num1 + num2;
                 break;
@@ -67,14 +69,26 @@ public class Calculadora {
                 int eleccion_conservar_numero = JOptionPane.showConfirmDialog(null, "¿Conservar el numero?", "Confirmacion",JOptionPane.YES_NO_OPTION);
                 conservar = (eleccion_conservar_numero == JOptionPane.YES_OPTION);
             }
+            operacion = num1+" "+operador+" "+num2+" = "+resultado;
+            historial.add(operacion);
             operador_validez = true;
         }
+        mostrarHistorial(historial);
         JOptionPane.showMessageDialog(null, "¡Gracias por usar la calculadora!");
     }
     public static double input_numero2(String texto){
         String numero_string = JOptionPane.showInputDialog(texto);
         double numero = Double.parseDouble(numero_string);
         return numero;
+    }
+    public static void mostrarHistorial(ArrayList<String> operaciones){
+        String stringMostrado = "";
+        int i = 1;
+        for (String n : operaciones){
+            stringMostrado += (i +". "+ n + "\n");
+            i++; 
+        }
+        JOptionPane.showMessageDialog(null, "---Historial---\n"+stringMostrado);
     }
     
 }
